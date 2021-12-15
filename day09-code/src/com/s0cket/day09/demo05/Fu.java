f0bd2e2836337283c2e129321a5384c7796a5223
qingDragon <921971961@qq.com>
2021-12-15 22:23:19 +0800

    2021-12-15-002

---
+++ day09-code/src/com/s0cket/day09/demo05/Fu.java
@@ -0,0 +1,7 @@
+package com.s0cket.day09.demo05;
+
+public class Fu {
+    String method() {
+        return null;
+    }
+}
---
+++ day09-code/src/com/s0cket/day09/demo05/Zi.java
@@ -0,0 +1,8 @@
+package com.s0cket.day09.demo05;
+
+public class Zi extends Fu {
+    @Override
+    public String method() {
+        return null;
+    }
+}
